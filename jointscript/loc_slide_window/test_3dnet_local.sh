e0729811280e2578d8d37569b11a4ac0926d0ecc
xiaolonw <dragon123@dragon123-M51AC.(none)>
2015-06-01 15:31:04 -0400

    update

--- jointscript/loc_slide_window/test_3dnet_local.sh
+++ jointscript/loc_slide_window/test_3dnet_local.sh
@@ -4,7 +4,8 @@
 
 ROOTFILE=/home/dragon123/3dnormal_joint_cnncode/caffe-3dnormal_joint
 
-GLOG_logtostderr=1  /home/dragon123/3dnormal_joint_cnncode/caffe-3dnormal_joint_past/build/tools/test_net_3dnormal_locim.bin /home/dragon123/3dnormal_joint_cnncode/caffe-3dnormal_joint_past/jointscript/loc_slide_window/seg_test_2fc_3dnormal.prototxt /home/dragon123/3dnormal_joint_cnncode/models/local_model/3dnormal__iter_280000  /home/dragon123/3dnormal_joint_cnncode/imgfile.txt /home/dragon123/3dnormal_joint_cnncode/slide_results
+GLOG_logtostderr=1  /home/dragon123/3dnormal_joint_cnncode/caffe-3dnormal_joint_past/build/tools/test_net_3dnormal_locim.bin /home/dragon123/3dnormal_joint_cnncode/caffe-3dnormal_joint_past/jointscript/loc_slide_window/seg_test_2fc_3dnormal.prototxt /home/dragon123/3dnormal_joint_cnncode/models/win_local_model/3dnormal__iter_280000  /home/dragon123/3dnormal_joint_cnncode/imgfile.txt /home/dragon123/3dnormal_joint_cnncode/slide_results
+
 
 
 
--- tools/test_net_3dnormal_locim.cpp
+++ tools/test_net_3dnormal_locim.cpp
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
 	}
 
 	Caffe::set_mode(Caffe::CPU);
-	//Caffe::SetDevice(0);
+//	Caffe::SetDevice(0);
 	NetParameter test_net_param;
 	ReadProtoFromTextFile(argv[1], &test_net_param);
 	Net<float> caffe_test_net(test_net_param);
